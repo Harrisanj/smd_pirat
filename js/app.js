01f8b539faa6fde6c311758333cf39a91204cc99
Harrisanj <55931677+Harrisanj@users.noreply.github.com>
2019-11-10 18:28:56 +0300

    Добавил строки

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var app = new Framework7({
+	
 	// App root element
 	root: '#app',
 	// App Name
@@ -27,10 +28,25 @@ var app = new Framework7({
 	  {
 		path: '/history',
 		url: 'pages/history.html',
+		on: {
+			pageInit: function (e, page) {
+				// do something when page initialized
+			},
+		}
 	  },
 	  {
 		path: '/add_event',
 		url: 'pages/add_event.html',
+		on: {
+			pageInit: function (e, page) {
+				var calendarModal = app.calendar.create({
+					inputEl: '#time_start',
+					openIn: 'customModal',
+					header: true,
+					footer: true,
+					});
+			},
+		}
 	  },
 	  {
 		path: '/add_person',
@@ -43,11 +59,38 @@ var app = new Framework7({
 	],
 	// ... other parameters
   });
-  
-  var mainView = app.views.create('.view-main');
-  var calendarModal = app.calendar.create({
-	inputEl: '#time_start',
-	openIn: 'customModal',
-	header: true,
-	footer: true,
-  });
+  var $$ = Dom7;
+	var mainView = app.views.create('.view-main');
+	var user_token = localStorage.getItem('user_token');
+if(user_token){
+	app.views.main.router.navigate('/history/', {reloadCurrent: true});}
+	
+	$$(document).on('click', '#login', function(){
+console.log("привет");
+var login = $$('#input-login').val();
+var password = $$('#input-password').val();
+console.log("login", login,'Пароль', password );
+/* Добавить ссылку -  */
+if (login=='' || password=='') {
+$$('#login_errors').text("Логин и пароль не могут быть пустыми");
+return 0;
+
+} 
+app.request.postJSON('https://smd.mos.ru/api/account/token', { login:login, password:password }, function (data) {
+	console.log(data);
+	localStorage.setItem('user_token', data.token);
+
+	app.views.main.router.navigate('/history/', {reloadCurrent: true});
+}, function (xhr, status) {
+	$$('#login_errors').text(xhr.responseText);
+}
+);
+
+
+
+
+
+
+
+	});
+
