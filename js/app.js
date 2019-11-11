a2a8c23527799e857d4a03bfdef162a407153751
Harrisanj <55931677+Harrisanj@users.noreply.github.com>
2019-11-11 12:46:10 +0300

    Добавлена история

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ var app = new Framework7({
 		url: 'pages/history.html',
 		on: {
 			pageInit: function (e, page) {
-				// do something when page initialized
+				getHistory();
 			},
 		}
 	  },
@@ -65,32 +65,67 @@ var app = new Framework7({
 if(user_token){
 	app.views.main.router.navigate('/history/', {reloadCurrent: true});}
 	
-	$$(document).on('click', '#login', function(){
-console.log("привет");
-var login = $$('#input-login').val();
-var password = $$('#input-password').val();
-console.log("login", login,'Пароль', password );
-/* Добавить ссылку -  */
-if (login=='' || password=='') {
-$$('#login_errors').text("Логин и пароль не могут быть пустыми");
-return 0;
+$$(document).on('click', '#login', function(){
+	console.log("привет");
+	var login = $$('#input-login').val();
+	var password = $$('#input-password').val();
+	console.log("login", login,'Пароль', password );
+	/* Добавить ссылку -  */
+	if (login=='' || password=='') {
+		$$('#login_errors').text("Логин и пароль не могут быть пустыми");
+		return 0;
+	} 
+	app.request.postJSON('https://smd.mos.ru/api/account/token', { login:login, password:password }, function (data) {
+		console.log(data);
+		localStorage.setItem('user_token', data.token);
 
-} 
-app.request.postJSON('https://smd.mos.ru/api/account/token', { login:login, password:password }, function (data) {
-	console.log(data);
-	localStorage.setItem('user_token', data.token);
-
-	app.views.main.router.navigate('/history/', {reloadCurrent: true});
-}, function (xhr, status) {
-	$$('#login_errors').text(xhr.responseText);
-}
+		app.views.main.router.navigate('/history/', {reloadCurrent: true});
+	},
+		function (xhr, status) {
+			$$('#login_errors').text(xhr.responseText);
+	}
 );
+});
 
-
-
-
-
-
+	$$(document).on('click', '#logout', function(){
+			localStorage.setItem('user_token', "");
+			/* localStorage.removeItem("user_token"); */
+			console.log("выход");
+		});
+var history_loaded = false;
+var history_data = [];
+function getHistory(){
+	if (history_loaded == true) {
+		build_history();
+		return 0;
+	}
+	history_loaded = true;
+	console.log("Получить историю");
+	app.request({
+		url: 'https://smd.mos.ru/api/mobile/activities/history',
+		dataType: "json",
+		headers: { 
+			'Authorization': 'Bearer '+localStorage.getItem('user_token')
+		},
+		success: function(data){
+			/* console.log(data); */
+			history_data=data;
+			build_history();
+		}
 
 	});
+}
 
+function build_history (){
+	for(i in history_data){
+		$$('#history_block').append('<li>\
+			<a href="/history_details?id='+history_data[i].id+'" class="item-link item-content">\
+			<div class="item-media"><i class="f7-icons">checkmark_seal_fill</i></div>\
+			<div class="item-inner">\
+			<div class="item-title">'+history_data[i].title+'</div>\
+			</div>\
+			</a>\
+			</li>');
+	}
+	/* console.log(history_data); */
+}
