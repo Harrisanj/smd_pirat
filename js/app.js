65c977ee32ff361019c219770ae9432e0d71e88b
Harrisanj <55931677+Harrisanj@users.noreply.github.com>
2019-11-14 16:50:00 +0300

    barcode4

--- js/app.js
+++ js/app.js
@@ -288,7 +288,7 @@ var app = new Framework7({
 					
 					cordova.plugins.barcodeScanner.scan(
 						function (result) {
-							alert("We got a barcode\n" +
+							$$("#test_logs").text("We got a barcode\n" +
 								  "Result: " + result.text + "\n" +
 								  "Format: " + result.format + "\n" +
 								  "Cancelled: " + result.cancelled);
@@ -297,15 +297,15 @@ var app = new Framework7({
 							alert("Scanning failed: " + error);
 						},
 						{
-							preferFrontCamera : true, // iOS and Android
-							showFlipCameraButton : true, // iOS and Android
-							showTorchButton : true, // iOS and Android
-							torchOn: true, // Android, launch with the torch switched on (if available)
+							preferFrontCamera : false, // iOS and Android
+							showFlipCameraButton : false, // iOS and Android
+							showTorchButton : false, // iOS and Android
+							torchOn: false, // Android, launch with the torch switched on (if available)
 							saveHistory: true, // Android, save scan history (default false)
-							prompt : "Place a barcode inside the scan area", // Android
-							resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+							prompt : "Наведите камеру на карту для считывания", // Android
+							resultDisplayDuration: 300, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
 							formats : "CODE_128", // default: all but PDF_417 and RSS_EXPANDED
-							orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+							orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
 							disableAnimations : true, // iOS
 							disableSuccessBeep: false // iOS and Android
 						}
